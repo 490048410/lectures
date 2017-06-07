b1c5ec62e919feacdbb8fa80653f9d6bf4fff031
Diba Mirza <dimirza@cs.ucsb.edu>
2017-06-07 15:20:51 -0700

    lecture code

--- 0607/practice/countPairs.cpp
+++ 0607/practice/countPairs.cpp
@@ -4,11 +4,20 @@
 using namespace std;
 
 
+int countPairs(char *str, int len){
+    if(len <3)
+        return 0;
+    if(str[0] == str[2])
+        return 1+countPairs(str+1, len-1);
+
+    return countPairs(str+1, len-1);
+
+}
 
 int main(int argc, char *argv[]){
     if(argc != 2){
         cerr<<"Usage : "<<argv[0]<<" <string>"<<endl;
         exit(1);
     }
-    cout<<"The number of pairs  = "<<endl;
+    cout<<"The number of pairs  = "<<countPairs(argv[1], strlen(argv[1]))<<endl;
 }
